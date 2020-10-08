baaf23d71496b3b22b2de57f69ec8d57fd74cc71
muhro <aj.atienza01@gmail.com>
2020-10-07 21:51:54 -0700

    Test Commit.

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -3,6 +3,8 @@ public class Problem1 {
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
         // Add your solution
+        System.out.println("Test Commit");
+
         return -1; // Place holder
     }
 }
